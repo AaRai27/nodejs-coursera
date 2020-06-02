50dd6cbc7c2ed878a2e625a30dceca74fe93da99
AaRai27 <46803391+AaRai27@users.noreply.github.com>
2020-06-02 09:17:58 +0700

    learning node js

---
+++ aboutMe.js
@@ -0,0 +1,3 @@
+exports.name = "Muhammad Afif Raihan";
+exports.birthDate = "27th November 2000";
+exports.sex = "Male";
---
+++ index.js
@@ -0,0 +1,3 @@
+var aboutMe = require('./aboutMe');
+
+console.log("Hello my name " + aboutMe.name + " i was born on " + aboutMe.birthDate + " and i'm a " + aboutMe.sex);
